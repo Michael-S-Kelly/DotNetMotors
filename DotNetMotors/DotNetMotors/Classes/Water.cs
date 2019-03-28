b974cb8782fe9db9bf891f0a640ac0d1076a8f65
Michael Kelly <mike.s.kelly@icloud.com>
2019-03-27 22:10:57 -0700

    Added base class & 3 child classes

---
+++ DotNetMotors/DotNetMotors/Classes/Water.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetMotors.Classes
+{
+    abstract class Water : Vehicles
+    {
+        public override string TravelMethod { get; set; } = "Water";
+        public abstract string WaterUse { get; set; }
+
+    }
+}
